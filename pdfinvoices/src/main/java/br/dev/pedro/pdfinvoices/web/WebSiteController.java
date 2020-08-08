fd9477725b8754b7fb1799b5596709f00b2f4c7f
Pedro Henrique <system.pedrohenrique@gmail.com>
2020-08-08 13:16:27 -0300

    adds login form

--- pdfinvoices/src/main/java/br/dev/pedro/pdfinvoices/web/WebSiteController.java
+++ pdfinvoices/src/main/java/br/dev/pedro/pdfinvoices/web/WebSiteController.java
@@ -1,8 +1,11 @@
 package br.dev.pedro.pdfinvoices.web;
 
+import br.dev.pedro.pdfinvoices.web.forms.LoginForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Date;
@@ -15,5 +18,20 @@ public class WebSiteController {
         model.addAttribute("username", username);
         model.addAttribute("currentDate", new Date());
         return "index.html";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("loginForm", new LoginForm());
+        return "login.html";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute LoginForm loginForm, Model model) {
+        if (loginForm.getUsername().equals(loginForm.getPassword())) {
+            return "redirect:/";
+        }
+        model.addAttribute("invalidCredentials", true);
+        return "login.html";
     }
 }
