a785e864992c0e2d6a75b78b0cb0c0e43fbc58a5
Pedro Henrique <system.pedrohenrique@gmail.com>
2020-08-08 13:09:52 -0300

    adds html models

--- pdfinvoices/src/main/java/br/dev/pedro/pdfinvoices/web/WebSiteController.java
+++ pdfinvoices/src/main/java/br/dev/pedro/pdfinvoices/web/WebSiteController.java
@@ -1,13 +1,19 @@
 package br.dev.pedro.pdfinvoices.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
 
 @Controller
 public class WebSiteController {
 
     @GetMapping("/")
-    public String homepage() {
+    public String homepage(Model model, @RequestParam(required = false, defaultValue = "stranger") String username) {
+        model.addAttribute("username", username);
+        model.addAttribute("currentDate", new Date());
         return "index.html";
     }
 }
