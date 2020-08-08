76a70fc4110a4b39bf3ad0064fc40563d034ddf3
Pedro Henrique <system.pedrohenrique@gmail.com>
2020-08-08 13:06:34 -0300

    configure view resolver

--- pdfinvoices/src/main/java/br/dev/pedro/pdfinvoices/context/ApplicationConfiguration.java
+++ pdfinvoices/src/main/java/br/dev/pedro/pdfinvoices/context/ApplicationConfiguration.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 /**
  * ComponentScan scans only the package and subpackages
@@ -44,4 +47,28 @@ public class ApplicationConfiguration {
         return new MethodValidationPostProcessor();
     }
 
+    @Bean
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+
+        viewResolver.setOrder(1);
+        viewResolver.setViewNames(new String[]{"*.html", ".xhtml"});
+        return viewResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("classpath:/templates/");
+        templateResolver.setCacheable(false);
+        return templateResolver;
+    }
 }
